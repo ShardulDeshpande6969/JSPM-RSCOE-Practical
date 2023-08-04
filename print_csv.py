977fae5c2f45476c1d62c2b57af496e20579f20f
ShardulDeshpande6969 <141392294+ShardulDeshpande6969@users.noreply.github.com>
2023-08-04 12:22:58 +0530

    Update print_csv.py

--- print_csv.py
+++ print_csv.py
@@ -2,3 +2,9 @@ import pandas as pd
 import io
 data = pd.read_csv(io.BytesIO(uploaded['selected-services-march-2023-quarter-csv (1).csv']))
 print(data)
+
+print("Head")
+data.head()
+
+print("Tail")
+data.tail()
